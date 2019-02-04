e8c885e4c6dcfb8893616ac56dc760c0e07133ca
raghuvanshraj <raghuvansh1997@gmail.com>
2019-02-04 12:17:04 +0530

    initial commit

---
+++ requirements.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+conda create -n Placement-Cell-Management-System python=3.6
+source activate Placement-Cell-Management-System
+pip install PyQt5==5.11.3
+pip install SQLAlchemy==1.2.17
