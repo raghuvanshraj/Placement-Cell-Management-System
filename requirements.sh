964c38d1d23aebdb14ccec66edb79a0a6d923d56
raghuvanshraj <raghuvansh1997@gmail.com>
2019-02-04 17:00:27 +0530

    added psycopg2-binary dependency for requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -4,3 +4,4 @@ conda create -n Placement-Cell-Management-System python=3.6
 source activate Placement-Cell-Management-System
 pip install PyQt5==5.11.3
 pip install SQLAlchemy==1.2.17
+pip install psycopg2-binary==2.7.7
