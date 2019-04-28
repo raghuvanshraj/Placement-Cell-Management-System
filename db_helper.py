f025140b104dc546116bb632d3a213823f8a6dec
Ridhima <bansal.ridhima30@gmail.com>
2019-04-28 15:18:23 +0530

    added functions to populate tables

--- db_helper.py
+++ db_helper.py
@@ -329,6 +329,202 @@ class DBHelper(object):
 
         return dict(zip(columns, result_restructured))
 
+    def populate_branches_eligible_table(self, filename):
+        import pandas as pd
+        import numpy as np
+        import os
+        path = os.path.join('csv_files', filename)
+        branch_eli = pd.read_csv(path)
+        branch_eli_np = np.array(branch_eli)
+
+        unique_branch_eli = []
+
+        for row in branch_eli_np:
+            if row[0] in unique_branch_eli:
+                continue
+
+            unique_branch_eli.append(row[0])
+            command = self.branches_eligible.insert().values(branch=row[0], course=row[1], job_id=row[2])
+            self.connection.execute(command)
+
+    def populate_companies_table(self, filename):
+        import pandas as pd
+        import numpy as np
+        import os
+        path = os.path.join('csv_files', filename)
+        companies_data = pd.read_csv(path)
+        companies_data_np = np.array(companies_data)
+
+        unique_company_data = []
+
+        for row in companies_data_np:
+            if row[0] in unique_company_data:
+                continue
+
+            unique_company_data.append(row[0])
+            command = self.companies.insert().values(company_tin=row[0], name=row[1], for_intern=row[2],
+                                                     for_placement=row[3], for_training=row[4], contact_no=row[5],
+                                                     email_id=row[6], location=row[7])
+            self.connection.execute(command)
+
+    def populate_jobs_table(self, filename):
+        import pandas as pd
+        import numpy as np
+        import os
+        path = os.path.join('csv_files', filename)
+        job = pd.read_csv(path)
+        job_np = np.array(job)
+
+        unique_jobs = []
+
+        for row in job_np:
+            if row[0] in unique_jobs:
+                continue
+
+            unique_jobs.append(row[0])
+            command = self.jobs.insert().values(job_id=row[0], job_title=row[1], description=row[2],
+                                                cutoff=row[3], deadline=row[4], dov=row[5],
+                                                process_ongoing=row[6],
+                                                package_placement=row[7], stipend_intern=row[8],
+                                                duration_intern=row[9], company_tin=row[10])
+            self.connection.execute(command)
+
+    def populate_login_credentials_student_table(self, filename):
+        import pandas as pd
+        import numpy as np
+        import os
+        path = os.path.join('csv_files', filename)
+        job = pd.read_csv(path)
+        job_np = np.array(job)
+
+        unique_jobs = []
+
+        for row in job_np:
+            if row[0] in unique_jobs:
+                continue
+
+            unique_jobs.append(row[0])
+            command = self.login_credentials_student.insert().values(username=row[0], password=row[1],
+                                                                     for_intern=row[2])
+            self.connection.execute(command)
+
+    def populate_login_credentials_company_table(self, filename):
+        import pandas as pd
+        import numpy as np
+        import os
+        path = os.path.join('csv_files', filename)
+        job = pd.read_csv(path)
+        job_np = np.array(job)
+
+        unique_jobs = []
+
+        for row in job_np:
+            if row[0] in unique_jobs:
+                continue
+
+            unique_jobs.append(row[0])
+            command = self.login_credentials_company.insert().values(username=row[0], password=row[1])
+            self.connection.execute(command)
+
+    def populate_student_applies_for_job_table(self, filename):
+        import pandas as pd
+        import numpy as np
+        import os
+        path = os.path.join('csv_files', filename)
+        job = pd.read_csv(path)
+        job_np = np.array(job)
+
+        unique_jobs = []
+
+        for row in job_np:
+            if row[0] in unique_jobs:
+                continue
+
+            unique_jobs.append(row[0])
+            command = self.student_applies_for_job.insert().values(roll_no=row[0], job_id=row[1],
+                                                                   is_shortlisted=row[2], is_selected=row[3])
+            self.connection.execute(command)
+
+    def populate_student_applies_for_training_table(self, filename):
+        import pandas as pd
+        import numpy as np
+        import os
+        path = os.path.join('csv_files', filename)
+        job = pd.read_csv(path)
+        job_np = np.array(job)
+
+        unique_jobs = []
+
+        for row in job_np:
+            if row[0] in unique_jobs:
+                continue
+
+            unique_jobs.append(row[0])
+            command = self.student_applies_for_training.insert().values(roll_no=row[0], training_id=row[1])
+            self.connection.execute(command)
+
+    def populate_student_manages_company_table(self, filename):
+        import pandas as pd
+        import numpy as np
+        import os
+        path = os.path.join('csv_files', filename)
+        job = pd.read_csv(path)
+        job_np = np.array(job)
+
+        unique_jobs = []
+
+        for row in job_np:
+            if row[0] in unique_jobs:
+                continue
+
+            unique_jobs.append(row[0])
+            command = self.student_manages_company.insert().values(roll_no=row[0], company_tin=row[1])
+            self.connection.execute(command)
+
+    def populate_students_table(self, filename):
+        import pandas as pd
+        import numpy as np
+        import os
+        path = os.path.join('csv_files', filename)
+        job = pd.read_csv(path)
+        job_np = np.array(job)
+
+        unique_jobs = []
+
+        for row in job_np:
+            if row[0] in unique_jobs:
+                continue
+
+            unique_jobs.append(row[0])
+            command = self.students.insert().values(roll_no=row[0], f_name=row[1], l_name=row[2], contact_no=row[3],
+                                                    address_line_1=row[4], address_line_2=row[5], address_line_3=row[6],
+                                                    pincode=row[7], gender=row[8], resume_uploaded=row[9],
+                                                    email_id=row[10], gpa_1=row[11], gpa_2=row[12], gpa_3=row[13],
+                                                    gpa_4=row[14], gpa_5=row[15], gpa_6=row[16], gpa_7=row[17],
+                                                    course=row[18], branch=row[19], category=row[20], grad_year=row[21],
+                                                    dob=row[22], no_of_backlogs=row[23], marks_10=row[24],
+                                                    marks_12=row[25], is_PC=row[26])
+            self.connection.execute(command)
+
+    def populate_trainings_table(self, filename):
+        import pandas as pd
+        import numpy as np
+        import os
+        path = os.path.join('csv_files', filename)
+        job = pd.read_csv(path)
+        job_np = np.array(job)
+
+        unique_jobs = []
+
+        for row in job_np:
+            if row[0] in unique_jobs:
+                continue
+
+            unique_jobs.append(row[0])
+            command = self.trainings.insert().values(training_id=row[0], subject_matter=row[1], date=row[2],
+                                                     time=row[3], company_tin=row[4])
+            self.connection.execute(command)
+
 
 if __name__ == '__main__':
     username = 'lruafctxgsjdsb'
